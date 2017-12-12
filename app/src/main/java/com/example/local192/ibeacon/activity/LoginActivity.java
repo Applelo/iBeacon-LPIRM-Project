1b7c99a847536f13c101b3bace5a4de29b8e96bc
Loïs <boubaultlois@gmail.com>
2017-12-12 10:15:28 +0100

    Config android manifest and project

---
+++ app/src/main/java/com/example/local192/ibeacon/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.local192.ibeacon.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.local192.ibeacon.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/local192/ibeacon/activity/StoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.local192.ibeacon.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.local192.ibeacon.R;
+
+public class StoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
