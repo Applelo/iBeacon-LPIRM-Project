049c3f5e9254ed3cca9c7a9ee47fc9591a648fe3
Ikenasio <ikenasio.m@hotmail.fr>
2017-12-12 15:01:11 +0100

    Mis en place du lectuer NFC

--- app/src/main/java/com/example/local192/ibeacon/activity/LoginActivity.java
+++ app/src/main/java/com/example/local192/ibeacon/activity/LoginActivity.java
@@ -1,17 +1,80 @@
 package com.example.local192.ibeacon.activity;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.local192.ibeacon.R;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class LoginActivity extends Activity {
 
+    ImageView imageNfc;
+    Handler handler = new Handler();
+    IntentFilter[] filters;
+    String[][] techs;
+    PendingIntent pendingIntent;
+    NfcAdapter nfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        startActivity(new Intent(LoginActivity.this, StoryActivity.class));
+        setTitle(R.string.login_text);
+        imageNfc = (ImageView) findViewById(R.id.imageNfc);
+        startAnim();
+        pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter intentFilter = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        filters = new IntentFilter[]{intentFilter};
+        techs = new String[][]{new String[]{NfcA.class.getName()}};
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startAnim();
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, techs);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        byte[] id = tag.getId();
+        ByteBuffer byteBuffer = ByteBuffer.wrap(id);
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        int sgnedInt = byteBuffer.getInt();
+        long number = sgnedInt & 0xfffffff1;
+        afficherInfo(number);
+    }
+
+    public void afficherInfo(long number) {
+        Log.e("Tag", number + "");
+    }
+
+    public void startAnim(){
+        imageNfc.clearAnimation();
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.image_anim);
+        imageNfc.setAnimation(animation);
+        imageNfc.animate();
     }
 }
